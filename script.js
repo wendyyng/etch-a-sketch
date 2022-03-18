7b363d7185dbb45486c5fda18d073ed3d2e2ca4a
Wendy <ng_yih_wei@hotmail.com>
2022-03-17 21:57:09 -0700

    Add html css js

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('div.container');
+let currentSize = 8;
+let squareStyle = document.createElement('style');
+
+function createSquares (size){
+  currentSize = size;
+  let string = "";
+  for (let i = 0; i < Math.pow(size, 2); i++){
+   string += '<div class="square"></div>';
+  }
+  container.innerHTML = string;
+  squareStyle.innerHTML = `.container {
+  grid-template-columns: repeat(${size}, 0fr);
+  grid-template-rows: repeat(${size}, 0fr);
+  }
+  .square {
+  width: ${(960/size)-(2)}px;
+  height: ${(960/size)-(2)}px;
+  }`
+  document.head.appendChild(squareStyle);
+  squareTransition();
+}
+
+
+function squareTransition (){
+  const squares = document.querySelectorAll("div.square");
+
+  squares.forEach(item => {
+  item.addEventListener('mousemove',  function() {
+  this.classList.add("transitioned");
+  })
+  }) 
+}
+
+createSquares(currentSize);
+
